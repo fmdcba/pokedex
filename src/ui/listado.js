06709f02445517256df40b156a37c28c4ae1a58d
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2024-04-22 21:35:32 -0300

    reemplaza css por clases de bootstrap

--- src/ui/listado.js
+++ src/ui/listado.js
@@ -12,7 +12,7 @@ export function agregarPokemonAlListado(pokemon, callbackSeleccionPokemon) {
   const $contenedorTexto = document.createElement('div');
   const $textoImagen = document.createElement('a');
 
-  $contenedor.classList.add('card', 'col-2', 'text-center');
+  $contenedor.classList.add('card', 'col-2', 'text-center', 'bg-warning');
   $contenedor.style.width = '150px';
   $contenedor.style.height = '170px';
 
@@ -46,6 +46,7 @@ export function mostrarPokemon(pokemon) {
   $imagen.setAttribute('src', pokemon.sprites.front_default);
   $imagen.setAttribute('alt', `imagen-del-pokemon-${pokemon.name}`);
   $nombre.textContent = pokemon.name;
+  $nombre.classList.add('text-uppercase');
   $numero.textContent = pokemon.order;
   $experiencia.textContent = pokemon.base_experience;
   $habilidades.innerHTML = '';
@@ -55,13 +56,13 @@ export function mostrarPokemon(pokemon) {
   pokemon.types.forEach((tipo) => {
     const $tipoTexto = document.createElement('span');
     $tipoTexto.textContent = tipo.type.name;
-    $tipoTexto.classList.add('badge', 'text-bg-danger', 'ps-2');
+    $tipoTexto.classList.add('badge', 'text-bg-danger', 'mx-2');
     $tipo.appendChild($tipoTexto);
   });
   pokemon.abilities.forEach((habilidad) => {
     const $habilidadTexto = document.createElement('span');
     $habilidadTexto.textContent = habilidad.ability.name;
-    $habilidadTexto.classList.add('badge', 'text-bg-warning', 'ps-2');
+    $habilidadTexto.classList.add('badge', 'text-bg-danger', 'mx-2');
     $habilidades.appendChild($habilidadTexto);
   });
 }
